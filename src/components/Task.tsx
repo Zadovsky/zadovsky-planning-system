b5d0718d3cdafd80081a9680d9dc9e7a71dbf4b9
Vladimir Zadovsky <vist1c@yandex.ru>
2023-05-04 17:07:09 +0400

    Made some Task styling

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,11 +1,12 @@
 import { ReactElement } from 'react';
+import styles from './Task.module.scss';
 
 interface ITask {
   title: String;
 }
 
 const Task = ({ title }: ITask): ReactElement => {
-  return <div>{title}</div>;
+  return <div className={styles.task}>{title}</div>;
 };
 
 export default Task;
